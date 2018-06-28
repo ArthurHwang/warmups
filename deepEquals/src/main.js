39ffd111e0fb27d297e0a8330c8a538a08e9eea3
ArthurHwang <ahwang55@gmail.com>
2018-06-28 10:56:19 -0700

    passed 8 test, 4 left

--- deepEquals/src/main.js
+++ deepEquals/src/main.js
@@ -1,3 +1,88 @@
 function deepEquals(x, y) {
 
+  // if (JSON.stringify(x) === JSON.stringify(y)) {
+  // return true
+  //
+  // }
+
+if (x instanceof Date && y instanceof Date) {
+  return x.getTime() === y.getTime() ? true : false
+
 }
+// if (Array.isArray(x) && Array.isArray(y)) {
+//   return true
+// }
+//
+if (Object.prototype.toString.call(x) ==
+    Object.prototype.toString.call(y)) {
+  return true
+}
+if (Object.prototype.toString.call(x).includes("Array") &&
+    Object.prototype.toString.call(y).includes("Array")) {
+  return true
+}
+
+// if (Object.getPrototypeOf(x) === Object.getPrototypeOf(y)) {
+//   return false
+// }
+// if(x == y) {
+//   return true
+// }
+// if (x != y) {
+//   return false
+// }
+// if  (x !== y ) {
+//   return false
+// }
+if(x === y) {
+  return true
+}
+if (x !== y) {
+  return false
+}
+
+
+
+
+
+
+}
+// if (typeof x === 'object' && typeof y === 'object') {
+//   return true
+// }
+// if (x == y) {
+//   return false
+// }
+// if (x instanceof Date && y instanceof Date) {
+//
+//   x.getTime() === y.getTime() ? true : false
+
+  // if (x == y) {
+  //   return false
+  // }
+
+// }
+
+
+
+
+
+// if (x.getTime() !== y.getTime()) {
+//   return false
+// }
+// if (!(x instanceof Date) && y instanceof Date) {
+//   return false
+// }
+// if (!Array.isArray(x) && Array.isArray(y)) {
+//   return false
+// }
+// if (Array.isArray(x) && Array.isArray(y)) {
+//   return true
+// }
+
+  // if (x == y) {
+  //   return true
+  // }
+  // if (x != y) {
+  //   return false
+  // }
